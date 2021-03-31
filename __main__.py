f098139355b6ce66bdbad785585f6140377ec3e6
Eliel <eliel.teotonio@gmail.com>
2021-03-31 08:23:38 +0100

    Added git ignore and updated the main file

--- __main__.py
+++ __main__.py
@@ -1,4 +1,4 @@
-#import ssh_tunnel
+import ssh_tunnel
 import argparse
 
 params_parser = argparse.ArgumentParser(description="Establishes a tunnel via SSH to a remote host")
@@ -12,5 +12,6 @@ args = params_parser.parse_args()
 
 if(args.remote_ip == None):
     params_parser.print_help()
-
-#tunnel = ssh_tunnel.ssh_tunnel(args.remote_ip,args.remote_port,args.dest_port)
+else:
+    tunnel = ssh_tunnel.ssh_tunnel(args.remote_ip,args.remote_port,args.dest_port)
+    tunnel.start_tunnel()
